620f1acdf3174c3ee1423a7eba4949309b02f3c5
Eluviian <bayleysapara@gmail.com>
2023-01-11 18:35:31 -0700

    added source

--- Lab1.py
+++ Lab1.py
@@ -5,6 +5,9 @@ raw_url = "https://raw.githubusercontent.com/Eluviian/CMPUT404Lab1/main/Lab1.py"
 #response = requests.get("https://google.com")
 #print(response.text)
 
+#source: https://stackoverflow.com/
+#from page: https://stackoverflow.com/questions/64430181/how-to-access-the-github-api-via-requests-in-python
+#user page:  https://stackoverflow.com/users/1581090/alex
 headers = {"Accept": "application/vnd.github.inertia-preview+json"}
 r = requests.get(raw_url, headers)
 
