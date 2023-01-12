14a2e2f749895c06b07020abab5895009d289d62
Eluviian <bayleysapara@gmail.com>
2023-01-11 18:33:08 -0700

    modified py file

--- Lab1.py
+++ Lab1.py
@@ -1,5 +1,12 @@
 import requests
+raw_url = "https://raw.githubusercontent.com/Eluviian/CMPUT404Lab1/main/Lab1.py"
 
-print(requests.__version__)
-response = requests.get("https://google.com")
-print(response.text)
+#print(requests.__version__)
+#response = requests.get("https://google.com")
+#print(response.text)
+
+headers = {"Accept": "application/vnd.github.inertia-preview+json"}
+r = requests.get(raw_url, headers)
+
+print(r.text)
+
